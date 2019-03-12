3bb82e7f66cd729516e31840d6cd6bf570544f16
Irina Tarelkina <itarelkina@yahoo.ca>
2019-03-12 18:46:09 +0000

    email in notification mailer removed

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,7 +4,6 @@ class NotificationMailer < ApplicationMailer
     def comment_added
         @place = comment.place
         @place_owner = @place.user
-        mail(to: @place_owner.email,
       mail(to: "my@email.com",
       subject: "A comment has been added to #{@place.name}")
     end
