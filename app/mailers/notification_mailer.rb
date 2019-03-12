892d03aeb88026505f8323ead67f47775537dc74
Irina Tarelkina <itarelkina@yahoo.ca>
2019-03-12 09:30:12 +0000

    Adjusting email mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,10 @@ class NotificationMailer < ApplicationMailer
     default from: "no-reply@nomsterapp.com"
   
     def comment_added
+        @place = comment.place
+        @place_owner = @place.user
+        mail(to: @place_owner.email,
       mail(to: "my@email.com",
-           subject: "A comment has been added to your place")
+      subject: "A comment has been added to #{@place.name}")
     end
   end
